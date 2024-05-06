12cb80e2c41e0e1226d8a33b35e635adcde5414b
Hansaja Hapuarachchi <138579096+hanzahapu@users.noreply.github.com>
2024-05-07 02:03:11 +0530

    Add files via upload

---
+++ Path.java
@@ -0,0 +1,11 @@
+class Path {
+    Location from, to;
+    String direction;
+
+    // Constructor
+    public Path(Location from, Location to, String direction) {
+        this.from = from;
+        this.to = to;
+        this.direction = direction;
+    }
+}
